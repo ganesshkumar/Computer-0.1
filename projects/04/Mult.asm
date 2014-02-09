8409fce585276e8db14e2d9171528e58e21b31f1
ganesshkumar <rpganesshkumar@gmail.com>
2014-02-09 18:21:05 +0530

    project 4 - multiplication

---
+++ projects/04/Mult.asm
@@ -0,0 +1,44 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/04/Mult.asm
+
+// Multiplies R0 and R1 and stores the result in R2.
+// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[3], respectively.)
+
+// Put your code here.
+
+// Setting R2 to zero
+@0
+D=A
+
+@R2
+M=D
+
+//Read R0 to D
+@R0
+D=M
+
+@END
+D; JLE
+
+(LOOP)
+@R2
+D=M
+
+@R1
+D=D+M
+@R2
+M=D
+
+@R0
+M=M-1
+D=M
+
+@LOOP
+D;JGT
+
+// infinite loop
+(END)
+@END
+0;JMP
